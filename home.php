cf1fb4867d4701791ec50a1abaf850f0999aabc0
adolfosucumula <rocketmc2009@gmail.com>
2023-07-18 01:31:12 +0100

    first commit

---
+++ home.php
@@ -0,0 +1,45 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+
+    <!---- swiper css link ----->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css" />
+
+    <!-- Font awesome cdn link --->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    
+    <!------ Custom css file link -->
+
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    
+<!---- header sectrion starts ----->
+<section class="header">
+    <a href="home.php" class="logo">XCode</a>
+
+    <nav class="navbar">
+        <a href="home.php">Home</a>
+        <a href="about.php">Home</a>
+        <a href="book.php">book</a>
+        <a href="package.php">package</a>
+        <a href="Projects.php">Projects</a>
+        <a href="contact.php">contact</a>
+    </nav>
+</section>
+
+<!----- header section ends ------>
+
+<!---- Swiper js file link --->
+
+<script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
+
+<!------ custom javascript file link ------>
+<script src="assets/js/script.js"></script>
+</body>
+</html>
