7e98f4c7b5072c3abca18410c0f223eed4cbf62e
adolfosucumula <rocketmc2009@gmail.com>
2023-07-19 13:05:29 +0100

    footer and Services2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,14 @@
 
 let menu = document.querySelector('#menu-btn1');
+let menu2 = document.querySelector('#menu-btn2');
 let navbar = document.querySelector('.header .navbar');
+let navbarLong = document.querySelector('.headerLong .navbarLong');
 let headerLong = document.querySelector('.headerLong');
 
 let i =0;
 menu.onclick = () => {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
-    headerLong.classList.toggle('active');
     i++;
     if(i==1){
         headerLong.style.display = "none";
@@ -15,4 +16,14 @@ menu.onclick = () => {
         i = 0;
         headerLong.style.display = "inline-block";
     }
+};
+
+menu2.onclick = () =>{
+    menu2.classList.toggle('fa-times');
+    navbarLong.classList.toggle('active');
 }
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+};
